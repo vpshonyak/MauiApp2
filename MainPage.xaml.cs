b90f1c30b231d05b39b5304afdde98b7a2c29a18
Vladimir Pshonyak <vpshonyak@msn.com>
2022-11-13 15:43:54 -0800

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace MauiApp2;
+
+public partial class MainPage : ContentPage
+{
+
+    public List<int> Numbers { get; } = Enumerable.Range(1, 100).ToList();
+    public MainPage()
+	{
+		InitializeComponent();
+        BindingContext = this;
+    }
+
+}
+
